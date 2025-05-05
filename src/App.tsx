1ef351867a2346844be761f2828701b365e28f8c
mahamathan02 <mahamathan2403@gmail.com>
2025-05-05 11:28:41 +0530

    changes: Project initiated & steup Comapleted with shandeCn UI

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { Button } from "./components/ui/button"
+
+
+const App = () => {
+  return (
+    <div className='text-blue-500'>
+      <Button variant={"outline"} size={"icon"}>hii</Button>
+      App
+    </div>
+  )
+}
+
+export default App
